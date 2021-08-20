b0e0d3aaedbce41c99049db3e36ad10eb25d9796
Anartz Mugika Ledo <mugan86@gmail.com>
2021-08-20 10:15:00 +0200

    Personalizando con marcadores custom

---
+++ assets/extends/icon-custom.js
@@ -0,0 +1,11 @@
+
+
+const MyIcon = L.Icon.extend({
+    options: {
+        iconSize: [38, 95],
+        iconAnchor: [22, 94],
+        popupAnchor: [-3, -76],
+        shadowSize: [68, 95],
+        shadowAnchor: [22, 94]
+    }
+});
