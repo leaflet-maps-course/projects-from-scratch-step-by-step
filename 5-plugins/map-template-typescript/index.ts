55512a62deaa83c0a02c26d31842d7088425ceda
Anartz Mugika Ledo <mugan86@gmail.com>
2021-08-29 12:39:22 +0200

    Trabajando con plugins de JS en typescript usando types

--- 5-plugins/map-template-typescript/index.ts
+++ 5-plugins/map-template-typescript/index.ts
@@ -1,8 +1,11 @@
+/// <reference path="./../../custom_typings/leaflet.browser.print/index.d.ts"/>
+// Arriba tenemos el fichero para "tipar" el uso de browserprint
 import { tileLayers } from "./../../constants/tile-layer";
 import { ATRIBUTION } from "./../../constants/general";
 import { control, Map, tileLayer } from "leaflet";
 
 import './../../node_modules/leaflet.browser.print/dist/leaflet.browser.print.min';
+
 const map = new Map("map", { center: [43.1736976, -2.4173474], zoom: 9 });
 
 tileLayer(tileLayers.default, {
